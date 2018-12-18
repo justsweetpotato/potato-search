85d1072690bbcb60d4ffbada4e7679b1035b58c1
sweetpotato <justsweetpotato@gmail.com>
2018-12-19 02:56:43 +0800

    修复搜索不存在的书籍时服务器500错误

--- potato/data.py
+++ potato/data.py
@@ -4,10 +4,13 @@
 def handle_data(s_msg):
     title_list = []
     link_list = []
-    for r_list in s_msg["items"]:
-        title_list.append(r_list["title"])
-        link_list.append(r_list["link"])
-    # content = {"titles": title_list, "links": link_list}
-    data_dict= dict(zip(title_list, link_list))
-    content = {"content": data_dict}
+    if s_msg.get("items"):  # 做判断, 以免输入不存在的书籍时服务器报500错误
+        for r_list in s_msg["items"]:
+            title_list.append(r_list["title"])
+            link_list.append(r_list["link"])
+        # content = {"titles": title_list, "links": link_list}
+        data_dict = dict(zip(title_list, link_list))
+        content = {"content": data_dict}
+    else:
+        content = None
     return content
--- potato/views.py
+++ potato/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import requests
 
 from .word import Word
@@ -23,16 +23,21 @@ def test(request):
         form = BookForm(request.POST)
         if form.is_valid():  # 验证表单数据
             c_msg = form.cleaned_data['text']  # 获取验证后的表单数据
-            # url = "https://www.googleapis.com/customsearch/v1?q={}&cx=007606540339251262492:fq_p2g_s5pa&num=10&start=1&key=AIzaSyDti_06GjeOV6trMz0ixATpXC6pTuJhAt4".format(
-            #     c_msg)
-            url = "https://www.googleapis.com/customsearch/v1?q={}&cx=007606540339251262492:fq_p2g_s5pa&num=10&start=1&key=AIzaSyCDw49epd-yMaZ1yfIwi7koM1AyZu8XzZ0".format(
+            url = "https://www.googleapis.com/customsearch/v1?q={}&cx=007606540339251262492:fq_p2g_s5pa&num=10&start=1&key=AIzaSyDti_06GjeOV6trMz0ixATpXC6pTuJhAt4".format(
                 c_msg)
+            url2 = "https://www.googleapis.com/customsearch/v1?q={}&cx=007606540339251262492:fq_p2g_s5pa&num=10&start=1&key=AIzaSyCDw49epd-yMaZ1yfIwi7koM1AyZu8XzZ0".format(
+                c_msg)  # 备用api一个api一天调用100次
             r = requests.get(url)
-            # s_msg = r.content.decode("utf-8")  返回 json
-            s_msg = r.json()  # 返回 python对象
-            content = handle_data(s_msg)
-            return render(request, 'detail.html', content)
-            # return HttpResponse(s_msg)
+            if r.status_code != 403:  # 表示第一个api 可用
+                # s_msg = r.content.decode("utf-8")  返回 json
+                s_msg = r.json()  # 返回 python对象
+                content = handle_data(s_msg)
+                return render(request, 'detail.html', content)
+                # return HttpResponse(s_msg)
+            else:
+                r = requests.get(url2)
+                s_msg = r.json()  # 返回 python对象
+                content = handle_data(s_msg)
+                return render(request, 'detail.html', content)
         else:
             return render(request, 'index2.html', content)
-
