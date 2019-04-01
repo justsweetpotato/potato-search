3fff0f558c6f3a2872649941d19c30546ec7abb8
sweetpotato <justsweetpotato@gmail.com>
2019-04-02 01:12:11 +0800

    优化向谷歌api请求逻辑,使用with自动关闭连接

--- potato/views.py
+++ potato/views.py
@@ -27,20 +27,22 @@ def index(request):
                 c_msg)
             url2 = "https://www.googleapis.com/customsearch/v1?q={}&cx=007606540339251262492:fq_p2g_s5pa&num=10&start=1&key=AIzaSyCDw49epd-yMaZ1yfIwi7koM1AyZu8XzZ0".format(
                 c_msg)  # 备用api一个api一天调用100次
-            r = requests.get(url)
-            if r.status_code != 403:  # 表示第一个api 可用
-                # s_msg = r.content.decode("utf-8")  返回 json
-                s_msg = r.json()  # 返回 Python对象
-                content = handle_data(s_msg)
-                return render(request, 'detail.html', content)
-                # return HttpResponse(s_msg)
-            else:
-                r = requests.get(url2)
-                if r.status_code != 403:
+            # r = requests.get(url)  # 使用此方法有时会报断开连接错误
+            with requests.get(url) as r:  # 使用 with 自动关闭连接
+                if r.status_code != 403:  # 表示第一个api 可用
+                    # s_msg = r.content.decode("utf-8")  返回 json
                     s_msg = r.json()  # 返回 Python对象
                     content = handle_data(s_msg)
                     return render(request, 'detail.html', content)
+                    # return HttpResponse(s_msg)
                 else:
-                    return HttpResponse("很抱歉, 今日api配额(200)已用完, 请明天再来.")
+                    # r = requests.get(url2)
+                    with requests.get(url2) as r:  # 使用 with 自动关闭连接
+                        if r.status_code != 403:
+                            s_msg = r.json()  # 返回 Python对象
+                            content = handle_data(s_msg)
+                            return render(request, 'detail.html', content)
+                        else:
+                            return HttpResponse("很抱歉, 今日api配额(200)已用完, 请明天再来.")
         else:
             return render(request, 'index.html', content)
