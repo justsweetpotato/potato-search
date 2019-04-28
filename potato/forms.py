009348fc6670f2353c9f10e2e831905387497f63
sweetpotato <justsweetpotato@gmail.com>
2019-04-28 18:26:41 +0800

    将表单最大输入增加到100

--- potato/forms.py
+++ potato/forms.py
@@ -4,4 +4,4 @@ from django import forms
 
 
 class BookForm(forms.Form):
-    text = forms.CharField(label="内容", required=True, max_length=50)
+    text = forms.CharField(label="内容", required=True, max_length=100)
