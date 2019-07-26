5754967d77c090a98b955bdde5873f6d2eadedd9
sweetpotato <justsweetpotato@gmail.com>
2019-07-27 04:33:18 +0800

    优化代码

--- potato/data.py
+++ potato/data.py
@@ -86,10 +86,11 @@ def requests_to_wikipedia(client_msg):
         else:
             continue
 
-    content_exist_list = re.match('.+? 可以指：', content)  # 词条存在多义吗
-    if content_exist_list:  # 如果词条存在多义
-        return None, None
     if content:
+        content_exist_list = re.match('.+? 可以指：', content)  # 词条存在多义吗
+        if content_exist_list:  # 如果词条存在多义
+            return None, None
+
         title = html.xpath('//*[@id="firstHeading"]/text()')[0]  # 获取标题
         content = re.sub('(\[.+?\]|（.*?）)', '', content)  # 将文本中的 [] 与 () 舍弃
         return title, content
