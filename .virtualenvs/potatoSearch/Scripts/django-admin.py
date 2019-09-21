a0bc96882cc9516d3797c2363497a50ec822b99b
little potato <justsweetpotato@gmail.com>
2019-09-21 16:04:46 +0800

    changed jpg

--- .virtualenvs/potatoSearch/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\data\code\search-book-heroku\.virtualenvs\potatosearch\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
